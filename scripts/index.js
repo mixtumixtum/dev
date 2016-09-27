c5357aac105bb35d57bf434acb5999be1361e632
yutaka_houlette <yutakahoulette@gmail.com>
2016-09-26 21:58:38 -0700

    Add roulette

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,10 @@ import flyd from 'flyd'
 import request from 'flyd-ajax'
 import render from 'flimflam-render'
 import snabbdom from 'snabbdom'
+import mix from 'array-shuffle'
+import loaded from 'imagesloaded'
+
+import dict from './image-dictionary'
 
 const flatMap = R.curry(require('flyd/module/flatmap'))
 
@@ -13,8 +17,35 @@ const init = _ => {
   return state
 }
 
+const attachImageLoaded = vnode => {
+  loaded(vnode.elm)
+    .on('always', x => {
+      let el = x.elements[0]
+      let height = x.images[0].img.offsetHeight
+      el.parentElement.style.height = height
+      el.style.opacity = 1
+      el.style.top = -height * Math.floor(Math.random() * (20 - 5) + 5)
+    })
+}
+
+const roulette = (state, ring) => {
+  const mixt = mix(dict[ring])
+  return h('div.col-3.p2.left.roulette-outer', [
+    h('div.roulette-inner'
+    , {hook: {insert: attachImageLoaded}}
+    , R.map(x => h('img' , { props: {src: `images/${ring}/${x}.jpg`} }) , mixt))
+  ])
+}
+
+
 const view = state => 
-  h('div', 'asdf') 
+  h('div.container.p1', [
+    h('div.clearfix', [
+      roulette(state, 'inner')
+    , roulette(state, 'middle')
+    , roulette(state, 'outer')
+    ])
+  ]) 
 
 let container = document.querySelector('#container')
 
