f45fc81d05f2dac8bff655422de79e03d38aa2ba
yutaka_houlette <yutakahoulette@gmail.com>
2016-09-29 21:40:43 -0700

    Add captions

--- scripts/index.js
+++ scripts/index.js
@@ -5,45 +5,60 @@ import flyd from 'flyd'
 import request from 'flyd-ajax'
 import render from 'flimflam-render'
 import snabbdom from 'snabbdom'
-import mix from 'array-shuffle'
+import shuffle from 'array-shuffle'
 import loaded from 'imagesloaded'
 
 import dict from './image-dictionary'
 
 const flatMap = R.curry(require('flyd/module/flatmap'))
 
+const log = (a, b) => flyd.map(x => console.log(a, x), b)
+
+const q = s => document.querySelectorAll(s)
+
+const style = (el, styles) => R.map(s => el.style[s[0]] = s[1], styles)
+
+
+const initSlot = h => {
+  const inner = q('.slot-inner') 
+  const outer = q('.slot-outer') 
+  const caption = q('.slot-caption') 
+  R.map(x => style(x, [['visibility', 'visible'],['height', `${h}px`]]), outer)
+  R.map(x => style(x, [['margin-top', `-${h * 31}px`]]), inner)
+  R.map(x => style(x, [['opacity', 1]]), caption)
+}
+
 const init = _ => {
   const state = {}
+  state.imagesLoaded$ = flyd.stream()
+  state.imageHeight$ = flyd.map(x => x.images[0].img.offsetHeight, state.imagesLoaded$) 
+  flyd.map(initSlot, state.imageHeight$) 
   return state
 }
 
-const attachImageLoaded = vnode => {
-  loaded(vnode.elm)
-    .on('always', x => {
-      let el = x.elements[0]
-      let height = x.images[0].img.offsetHeight
-      el.parentElement.style.height = height
-      el.style.opacity = 1
-      el.style.top = -height * Math.floor(Math.random() * (20 - 5) + 5)
-    })
-}
+const attachImageLoaded = state => vnode => 
+  loaded(vnode.elm).on('always',  state.imagesLoaded$)
 
-const roulette = (state, ring) => {
-  const mixt = mix(dict[ring])
-  return h('div.col-3.p2.left.roulette-outer', [
-    h('div.roulette-inner'
-    , {hook: {insert: attachImageLoaded}}
-    , R.map(x => h('img' , { props: {src: `images/${ring}/${x}.jpg`} }) , mixt))
+const slot = (state, col) => {
+  const imageNames = shuffle(dict[col])
+  return h('div.col-4.inline-block'
+  , [ h('div.slot-outer', [
+        h('div.slot-inner'
+      , R.map(x => h('img' , {props: {src: `images/${col}/${x}.jpg`}}) , imageNames))
+      ])
+    , h('p.slot-caption.center', R.last(imageNames)) 
   ])
 }
 
 
 const view = state => 
   h('div.container.p1', [
-    h('div.clearfix', [
-      roulette(state, 'inner')
-    , roulette(state, 'middle')
-    , roulette(state, 'outer')
+    h('div.slots'
+  , {hook: {insert: attachImageLoaded(state)}}
+  , [
+      slot(state, 'inner')
+    , slot(state, 'middle')
+    , slot(state, 'outer')
     ])
   ]) 
 
